6a73d5b3b9c6695a70e7b6e832ae949fc352918c
jiadx1 <jiadexing@sansec.cn>
2025-03-24 17:25:06 +0800

    fix 修复 PKCS#12 文件解析错误

--- pkcs12/pkcs12.go
+++ pkcs12/pkcs12.go
@@ -120,6 +120,9 @@ func ToPEM(pfxData []byte, password string) ([]*pem.Block, error) {
 	}
 
 	bags, encodedPassword, err := getSafeContents(pfxData, encodedPassword)
+	if err != nil {
+		return nil, err
+	}
 
 	blocks := make([]*pem.Block, 0, len(bags))
 	for _, bag := range bags {
